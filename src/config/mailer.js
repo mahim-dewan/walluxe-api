314d6f1b5d3343011b39b046ca90e363bdd4032f
Mahim Dewan <mahimdewan79@gmail.com>
2025-10-26 23:48:40 +0600

    Update mailer.js

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -2,12 +2,13 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 10000, // 10 seconds
 });
 
 module.exports = transporter;
